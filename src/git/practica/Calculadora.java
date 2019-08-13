e09d25f4c9dbd6fc72bea57ebfff967fce186a8c
PaulaColque <paulajulieta@live.com.ar>
2019-08-13 11:23:44 -0300

    agrego clase Calculadora y modifico main

---
+++ src/git/practica/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.practica;
+
+/**
+ *
+ * @author Colque
+ */
+public class Calculadora {
+    
+    int sumar(int a, int b){
+        int c = 0;
+        c = a +b;
+        return c;
+    }
+    int restar(int a, int b){
+        int c = 0;
+        c = a -b;
+        return c;
+    }
+    int multiplicar(int a, int b){
+        int c = 0;
+        c = a *b;
+        return c;
+    }
+    float dividir(float a, float b){
+        float c = 0;
+        c = a /b;
+        return  c;
+    }
+    
+}
--- src/git/practica/GitPractica.java
+++ src/git/practica/GitPractica.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package git.practica;
 
 /**
@@ -15,7 +11,8 @@ public class GitPractica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Calculadora c = new Calculadora();
+       System.out.println("resultado: " + c.sumar(5, 6) );
     }
     
 }
