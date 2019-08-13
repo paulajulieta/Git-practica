152b981194f72d8d6b853f27f341b0af6d299bff
PaulaColque <paulajulieta@live.com.ar>
2019-08-13 11:41:23 -0300

    agrego las clases Potencia y Raiz

---
+++ src/git/practica/Raiz.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.practica;
+
+/**
+ *
+ * @author Colque
+ */
+public class Raiz {
+    double raiz(int a){
+        double b=Math.sqrt(a);
+        return b;
+    }
+}
